4b90f46c075cb775e059e6af35fab1e5c348e7f0
AnnaNowak96 <jankowska.anna-96@wp.pl.noreply.github.com>
2024-01-11 15:57:52 +0100

    add buttons, add black and rainbow color and restart

--- main.js
+++ main.js
@@ -7,15 +7,59 @@ function addGrid() {
 }
 }
 
-const color = "#000000";
-const pixel = document.getElementsByClassName("gridBox");
-
-function colorPixel(event) {
-    event.target.style.background = color;
+function colorGridBlack() {
+const grids = document.getElementsByClassName("gridBox");
+for (let x=0; x<grids.length; x++) {
+    grids[x].addEventListener("mouseover", function() {
+        this.style.backgroundColor = "black";
+    })
+}
 }
 
-for (i=0; i<pixel.length; i++) {
-    pixel[i].addEventListener("mouseover", colorPixel);
+
+// function rainbowColor () {
+//     const rgbMax = 255;
+//     const a = Math.floor(Math.random()*rgbMax);
+//     const b = Math.floor(Math.random()*rgbMax);
+//     const c = Math.floor(Math.random()*rgbMax);
+//     const randomColor = (a, b, c);
+//     const grids = document.getElementsByClassName("gridBox");
+// for (let x=0; x<grids.length; x++) {
+//     grids[x].addEventListener("mouseover", function() {
+//         this.style.backgroundColor = randomColor;
+//     })
+// }
+// }
+
+
+function colorGridRainbow() {
+const colors = ["#6f2041","#982c52","#d74167","#ff6f8d",
+"#ffa3b2","#ffb68d","#ffdb6b","#0971a6","#08b599","#04f395"];
+const numberColors = parseInt(colors.length);
+const randomColor = colors[Math.floor(Math.random() * numberColors)];
+
+const grids = document.getElementsByClassName("gridBox");
+for (let x=0; x<grids.length; x++) {
+    grids[x].addEventListener("mouseover", function() {
+        const randomColor = colors[Math.floor(Math.random() * numberColors)];
+        this.style.backgroundColor = randomColor;
+    })
+}
 }
 
-window.addEventListener("load", addGrid);
+function restartGridBox() {
+    location.reload();
+}
+
+function changeSize() {
+    
+}
+
+document.getElementById("black").addEventListener("click", colorGridBlack);
+document.getElementById("rainbow").addEventListener("click", colorGridRainbow);
+document.getElementById("restart").addEventListener("click",restartGridBox);
+document.getElementById("size").addEventListener("click", changeSize);
+
+addGrid();
+
+
